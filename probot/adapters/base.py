ed6a3fed2476b94540eb6b4d0b51e0d02bd62ed1
Andrew Hawker <andrew.r.hawker@gmail.com>
2020-10-13 09:09:15 -0700

    Add Adapter abstract base class

---
+++ probot/adapters/base.py
@@ -0,0 +1,25 @@
+"""
+    probot/adapters/base
+    ~~~~~~~~~~~~~~~~~~~~
+
+    Contains base types to be used/extended by adapters.
+"""
+import abc
+from typing import Generic
+
+from .. import defaults
+from ..hints import AppT, RequestT, ResponseT
+
+
+class Adapter(Generic[AppT, RequestT, ResponseT],
+              metaclass=abc.ABCMeta):
+    """
+    Abstract adapter for handling HTTP requests/responses.
+    """
+    methods = [defaults.METHOD]
+
+    def __init__(self,
+                 app: AppT,
+                 path: str = defaults.PATH) -> None:
+        self.app = app
+        self.path = path
