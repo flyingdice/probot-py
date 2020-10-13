faeaede60316f306e035f40fb15e585690ba2b37
Andrew Hawker <andrew.r.hawker@gmail.com>
2020-10-13 08:59:54 -0700

    Add hints module
    This contains type hints that are used across the package.

---
+++ probot/hints.py
@@ -0,0 +1,53 @@
+"""
+    probot/hints
+    ~~~~~~~~~~~~
+
+    Contains type hints used across the package.
+"""
+from typing import Awaitable, Callable, Optional, TypeVar
+
+from . import models
+
+AdapterT = TypeVar('AdapterT')
+AppT = TypeVar('AppT')
+RequestT = TypeVar('RequestT')
+ResponseT = TypeVar('ResponseT')
+
+
+# Type alias for the return value of user defined handler functions.
+EventHandlerResponse = Optional[models.Response]
+
+# Type definition for async user defined handler functions on a probot app that
+# takes context objects and returns optional responses.
+AsyncEventHandler = Callable[[models.Context], Awaitable[EventHandlerResponse]]
+
+# # Type definition for a async registered user defined handler function
+# # for webhook events.
+# RegisteredAsyncEventHandler = Callable[[AsyncEventHandler], AsyncEventHandler]
+
+# Type definition for sync user defined handler functions on a probot app that
+# takes context objects and returns optional responses.
+SyncEventHandler = Callable[[models.Context], EventHandlerResponse]
+
+# Type definition for async or sync event handlers.
+EventHandlerT = TypeVar('EventHandlerT', AsyncEventHandler, SyncEventHandler)
+
+# Type definition for a sync registered user defined handler function
+# for webhook events.
+RegisteredEventHandler = Callable[[EventHandlerT], EventHandlerT]
+
+# Type definition for async handler functions that take probot requests
+# and return probot response objects.
+ProbotAsyncHandler = Callable[[models.Request], Awaitable[models.Response]]
+
+# Type definition for async adapter handler functions that wrap native handlers
+# and handle adapter specific request/response objects.
+AdapterAsyncHandler = Callable[[RequestT], Awaitable[ResponseT]]
+
+# Type definition for sync handler functions that take probot requests
+# and return probot response objects.
+ProbotSyncHandler = Callable[[models.Request], models.Response]
+
+# Type definition for sync adapter handler functions that wrap native handlers
+# and handle adapter specific request/response objects.
+AdapterSyncHandler = Callable[[RequestT], ResponseT]
