504b76fe1a9f1ca681448763b8f7e859b5a25a4a
Andrew Hawker <andrew.r.hawker@gmail.com>
2020-10-13 16:30:52 -0700

    Add common functionality to base/App

--- probot/base.py
+++ probot/base.py
@@ -1,14 +1,15 @@
 """
-    probot/adapters/base
-    ~~~~~~~~~~~~~~~~~~~~
+    probot/base
+    ~~~~~~~~~~~
 
     Contains abstract base types to be extended.
 """
 import abc
 import collections
+import hmac
 from typing import Callable, Dict, Generic, List, Optional, Type, TypeVar
 
-from . import config, errors, defaults, models
+from . import config, defaults, errors, github, models
 from .hints import AdapterAppT, AdapterRequestT, AdapterResponseT, EventHandlerT
 
 
@@ -40,6 +41,7 @@ class App(Generic[AdapterT, EventHandlerT],
         self.adapter.register(self.on_request)
         self.handlers: Dict[models.ID, List[EventHandlerT]] = collections.defaultdict(list)
         self.app_id = None
+        self.private_key = None
         self.webhook_secret = None
 
     def configure(self, conf: config.Config) -> None:
@@ -50,6 +52,7 @@ class App(Generic[AdapterT, EventHandlerT],
         :return: Nothing
         """
         self.app_id = conf.app_id
+        self.private_key = conf.private_key
         self.webhook_secret = conf.webhook_secret
 
     def register_handler(self,
@@ -93,6 +96,110 @@ class App(Generic[AdapterT, EventHandlerT],
         :return: Response
         """
         raise NotImplementedError('Must be implemented by derived class')
+
+    @staticmethod
+    def create_context(event: models.EventT,
+                       app_id: str,
+                       private_key: str) -> models.Context:
+        """
+        Create context for the given webhook event.
+
+        :param event: Event to create context for
+        :param app_id: ID of the GitHub App
+        :param private_key: PEM of the GitHub App
+        :return: Context
+        """
+        return models.Context(
+            event=event,
+            github=github.create_github_installation_api(event, app_id, private_key)
+        )
+
+    @staticmethod
+    def parse_request(request: models.Request) -> models.EventT:
+        """
+        Parse the given request into an event.
+
+        :param request: Request to parse
+        :return: Event
+        """
+        delivery_id = request.headers['X-GitHub-Delivery']
+        event_name = request.headers['X-GitHub-Event']
+        hook_id = int(request.headers['X-GitHub-Hook-ID'])
+        payload = request.body_json
+        action = payload.pop('action', None)
+        return models.new_event(delivery_id, event_name, hook_id, action, payload)
+
+    def verify_request(self,
+                       request: models.Request,
+                       app_id: str,
+                       webhook_secret: str) -> None:
+        """
+        Verify the given request.
+
+        If unverified, raise an HTTP 401 Unauthorized exception.
+
+        :param request: Request to verify
+        :param app_id: Expected GitHub app id
+        :param webhook_secret: Shared webhook secret between GitHub and GitHub App
+        :return: Nothing
+        """
+        self.verify_request_signature(request, webhook_secret)
+        self.verify_request_target_id(request, app_id)
+
+    @staticmethod
+    def verify_request_signature(request: models.Request,
+                                 webhook_secret: str) -> None:
+        """
+        Verify the given request originated from GitHub.
+
+        If unverified, raise an HTTP 401 Unauthorized exception.
+
+        :param request: Request to validate
+        :param webhook_secret: Shared webhook secret between GitHub and GitHub App
+        :return: Nothing
+        """
+        algorithm, digest = request.headers['X-Hub-Signature'].split('=')
+
+        obj = hmac.new(
+            key=webhook_secret.encode('utf-8'),
+            msg=request.body_raw,
+            digestmod=algorithm
+        )
+
+        match = hmac.compare_digest(digest, obj.hexdigest())
+        if not match:
+            raise errors.HTTPException(401, 'Invalid webhook signature')
+
+    @staticmethod
+    def verify_request_target_id(request: models.Request,
+                                 app_id: str) -> None:
+        """
+        Verify the given request is for the expected GitHub App.
+
+        If unverified, raise an HTTP 401 Unauthorized exception.
+
+        :param request: Request to validate
+        :param app_id: Expected GitHub app id
+        :return: Nothing
+        """
+        target_id = request.headers.get('X-GitHub-Hook-Installation-Target-ID')
+        if not target_id:
+            raise errors.HTTPException(401, 'Missing installation target id')
+        if target_id != app_id:
+            raise errors.HTTPException(401, 'Installation target id mismatch')
+
+    @staticmethod
+    def wrap_response(result: Optional[models.Response]) -> models.Response:
+        """
+        Wrap the result of a handler execution into a Response object.
+
+        If the handler returned a response, use that.
+        If the handler did not return a response, return a 200 OK response.
+
+        :param result: Result of a handler execution
+        :return: Response
+        """
+        return result if result else models.Response(status_code=200)
 
 
 # Type alias for :class:`~probot.base.App` derived classes.
